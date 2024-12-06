db632cac3d078688af8226d5fb39641707037ade
fadhilsyauqy <rahmatfadhilsauqy08@gmail.com>
2024-12-06 23:23:02 +0700

    Update server to work with Dockerfile

--- src/server/server.js
+++ src/server/server.js
@@ -6,8 +6,8 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
     const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
+        port: 8080,
+        host: '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
